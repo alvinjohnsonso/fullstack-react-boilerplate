0bf6608d495d82d9a790c325368382ad9e042817
Alvin Johnson So <so.alvinjohnson@gmail.com>
2020-07-05 01:16:44 +0800

    added docker
    Signed-off-by: Alvin Johnson So <so.alvinjohnson@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:current-slim
+WORKDIR /app
+COPY package.json .
+
+# install all packages first
+RUN npm install
+
+COPY . .
+
+RUN if [ "${NODE_ENV}" != "development" ]; then npm run build; fi
+
+CMD if [ "${NODE_ENV}" != "development" ]; then npm start; else npm run dev; fi
--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const api = require("./api")
 const app = express();
 
 // middlewares
 app.use(morgan("common"));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
+app.use("/api/v1", api);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
